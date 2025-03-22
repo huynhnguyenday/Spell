5f1c5477feabe9305b261f96571ef2c247bf8c66
huynhnguyenday <nguyenhuuhuynh2702@gmail.com>
2025-03-22 08:49:07 +0700

    Revert "Add backend integration (#7)"
    This reverts commit 544d45f45b939f468c6e87ba27a6099b6bb4678c.

--- backend/src/models/image.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const ImageSchema = new mongoose.Schema({
-  egg: { type: String, required: true },
-  example: { type: String, required: true },
-});
-
-const Image = mongoose.model("Image", ImageSchema);
-export default Image;
